4a52be6464d56cd4977c3d09bdb1c2f2a935ff73
dalyoon <yeodal89@gmail.com>
2018-08-10 14:46:43 +0900

    #2.8 Typescript and Styled Components part Two

---
+++ notes/EN/2_8_Typescript_and_Styled_Components_part_Two.md
@@ -0,0 +1,25 @@
+# 2.8 Typescript and Styled Components part Two
+
+## section.log
+
+- How to reset default style with `styled-reset`
+
+## tips
+
+- in code, `// tslint:disable-next-line` comment disables all rules for the following line<br>
+  make sure there is no space after colon`:`
+
+## issue
+
+## links
+
+- [styled-reset](https://github.com/zacanger/styled-reset)
+- [tslink rule flag](https://palantir.github.io/tslint/usage/rule-flags/)
+
+## added dependencies
+
+### dependencies
+
+- styled-reset
+
+### devDependencies
--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { graphql } from "react-apollo";
 
+import { reset } from "styled-reset";
+import theme from "../../theme";
+import { injectGlobal, ThemeProvider } from "../../typed-components";
 import AppPresenter from "./AppPresenter";
 import { IS_LOGGED_IN } from "./AppQueries";
 
+// tslint:disable-next-line
+injectGlobal`
+  ${reset}
+`;
+
 const AppContainer = ({ data }) => {
   const { isLoggedIn } = data.auth;
-  return <AppPresenter isLoggedIn={isLoggedIn} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <AppPresenter isLoggedIn={isLoggedIn} />
+    </ThemeProvider>
+  );
 };
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
