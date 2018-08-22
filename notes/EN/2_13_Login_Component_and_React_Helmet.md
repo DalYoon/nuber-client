daa791c459275778fdb6dcaa74884fd5de315321
dalyoon <yeodal89@gmail.com>
2018-08-22 12:47:19 +0900

    #2.13 Login Component and React Helmet

---
+++ notes/EN/2_13_Login_Component_and_React_Helmet.md
@@ -0,0 +1,20 @@
+# 2.13 Login Component and React Helmet
+
+## section.log
+
+- basic css work for design
+- change title with `react-helmet`
+
+## tips
+
+## issue
+
+## links
+
+## added dependencies
+
+### dependencies
+
+- react-helmet
+
+### devDependencies
--- src/Components/App/AppPresenter.tsx
+++ src/Components/App/AppPresenter.tsx
@@ -6,7 +6,7 @@ import AddPlace from "../../Routes/AddPlace";
 import EditAccount from "../../Routes/EditAccount";
 import FindAddress from "../../Routes/FindAddress";
 import Home from "../../Routes/Home";
-import OutHome from "../../Routes/OutHome";
+import Login from "../../Routes/Login";
 import PhoneLogin from "../../Routes/PhoneLogin";
 import Places from "../../Routes/Places";
 import Ride from "../../Routes/Ride";
@@ -25,7 +25,7 @@ const AppPresenter: React.SFC<IProps> = ({ isLoggedIn }) => {
 const LoggedOutRoutes: React.SFC = () => {
   return (
     <Switch>
-      <Route path={"/"} exact={true} component={OutHome} />
+      <Route path={"/"} exact={true} component={Login} />
       <Route path={"/phone-login"} exact={true} component={PhoneLogin} />
       <Route path={"/verify-phone/:number"} exact={true} component={VerifyPhone} />
       <Route path={"/social-login"} exact={true} component={SocialLogIn} />
